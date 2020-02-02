6cce1210d5646449d1e11b8c25ff8d1aec9c2bb2
Iku Iwasa <iku.iwasa@gmail.com>
2020-02-02 23:57:45 +0900

    Generate README from bucket files

---
+++ gen-readme.ps1
@@ -0,0 +1,22 @@
+$result = "# Personal Scoop bucket
+
+[![Build status](https://ci.appveyor.com/api/projects/status/sdwq9tekqddjawo7/branch/master?svg=true)](https://ci.appveyor.com/project/iquiw/scoop-bucket/branch/master)
+
+## Applications
+
+| Name | Description |
+| ---  | ---         |
+"
+
+$files = Get-ChildItem bucket
+
+foreach ($file in $files) {
+    $path = Join-Path -Path "bucket" -ChildPath $file
+    $name = $file -replace "\.json$", ""
+    $json = Get-Content $path | ConvertFrom-Json
+    $homepage = $json.homepage
+    $descr = $json.description
+    $result += "| [$name]($homepage) | $descr |`r`n"
+}
+
+$result | Out-File README.md
